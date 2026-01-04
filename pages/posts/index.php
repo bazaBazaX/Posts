3a8c0a4eb4abaffa3aafdbbb9f25aa8fc2d55d80
andriy <you@example.com>
2026-01-04 23:40:07 +0200

    posts page (undone if i add createPosts to there)

--- pages/posts/index.php
+++ pages/posts/index.php
@@ -6,7 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posts</title>
     <link rel="stylesheet" href="/include/styles/header.css">
+    <link rel="stylesheet" href="/pages/posts/style/style.css">
 </head>
 <body>
+    <main>
+        <h2>Posts</h2>
+        <section>
+            <h1>Admin</h1>
+            <p>Baza is da best programmer in the entire globe.</p>
+            <div class="time">
+                <h3>04.01.2026</h3>
+                <h4>23:12</h4>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
